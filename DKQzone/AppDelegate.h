b03e149876e3b66dcba9fa770bf2acdfd87766cd
kevin <929118967@qq.com>
2016-12-17 19:22:28 +0800

    Initial Commit

---
+++ DKQzone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DKQzone
+//
+//  Created by devzkn on 17/12/2016.
+//  Copyright © 2016 hisun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DKQzone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DKQzone
+//
+//  Created by devzkn on 17/12/2016.
+//  Copyright © 2016 hisun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
